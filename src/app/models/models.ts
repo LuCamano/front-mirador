31bc5991f1f543f5985df844436b732a2b4f92ba
Luciano <lu.camano@duocuc.cl>
2024-12-12 06:04:22 -0300

    feat: agregar interfaces para Departamento, Gasto, Pago, PagoRealizado y Servicio

--- src/app/models/models.ts
+++ src/app/models/models.ts
@@ -4,3 +4,42 @@ export interface Edificio {
   nombre: string;
   numPisos: number;
 }
+
+export interface Departamento {
+  estado: 'Habitado' | 'Desocupado';
+  idDepartamento?: number;
+  idEdificio: number;
+  numero: number;
+  piso: number;
+}
+
+export interface Gasto {
+  descripcion: string;
+  estado: 'pendiente' | 'pagado';
+  fecha: Date;
+  idDepartamento: number;
+  idGasto?: number;
+  tipo: string;
+  valor: number;
+}
+
+export interface PagoRealizado {
+  gasto: Gasto;
+  pagos: Pago[];
+  'por-pagar': number;
+  'total-pagado': number;
+}
+
+export interface Pago {
+  fechaPago: Date;
+  idGasto: number;
+  idPago?: number;
+  montoPagado: number;
+}
+
+export interface Servicio {
+  idServicio?: number; 
+  nombre: string;
+  descripcion: string;
+  idGasto: number;
+}
